6c747124a7f951853a0893ceac7e3e86f90b84ec
Nick Logozzo <nlogozzo225@gmail.com>
2025-11-28 12:11:30 -0500

    feat: Add GNOME helper library

---
+++ Nickvision.Desktop.GNOME/Controls/ShortcutsDialog.cs
@@ -0,0 +1,18 @@
+﻿using Nickvision.Desktop.Globalization;
+using Nickvision.Desktop.GNOME.Helpers;
+
+namespace Nickvision.Desktop.GNOME.Controls;
+
+public class ShortcutsDialog
+{
+    private readonly Gtk.Builder _builder;
+    private readonly Adw.ShortcutsDialog _shortcutsDialog;
+
+    public ShortcutsDialog(ITranslationService translator)
+    {
+        _builder = Gtk.Builder.NewFromBlueprint("ShortcutsDialog", translator);
+        _shortcutsDialog = new Adw.ShortcutsDialog(new Adw.Internal.ShortcutsDialogHandle(_builder.GetPointer("root"), false));
+    }
+
+    public void Present(Gtk.Widget? parent) => _shortcutsDialog.Present(parent);
+}
