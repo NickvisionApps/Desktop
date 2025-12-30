2879c0c5e8660ad6c753051a27af0587b6b3f707
Nick Logozzo <nlogozzo225@gmail.com>
2025-12-30 12:13:55 -0500

    V2025.12.1-winui

---
+++ Nickvision.Desktop.WinUI/Helpers/ComboBoxExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Nickvision.Desktop.Application;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nickvision.Desktop.WinUI.Helpers;
+
+public static class ComboBoxExtensions
+{
+    extension(ComboBox comboBox)
+    {
+        public void SelectSelectionItem()
+        {
+            if (comboBox.ItemsSource is IReadOnlyList<ISelectionItem> items)
+            {
+                comboBox.SelectedItem = items.FirstOrDefault(item => item.ShouldSelect);
+            }
+        }
+    }
+}
---
+++ Nickvision.Desktop.WinUI/Helpers/ListViewExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Nickvision.Desktop.Application;
+using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Nickvision.Desktop.WinUI.Helpers;
+
+public static class ListViewExtensions
+{
+    extension(ListView listView)
+    {
+        public void SelectSelectionItems()
+        {
+            if (listView.ItemsSource is IReadOnlyList<ISelectionItem> items)
+            {
+                foreach (var item in items.Where(i => i.ShouldSelect))
+                {
+                    listView.SelectedItems.Add(item);
+                }
+            }
+        }
+    }
+}
