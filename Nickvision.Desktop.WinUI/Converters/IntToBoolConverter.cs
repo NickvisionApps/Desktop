1d3e3bd6d7dd715e33a977bcc7df5012c1457b33
Nick Logozzo <nlogozzo225@gmail.com>
2025-12-30 20:24:06 -0500

    V2025.12.2-winui

---
+++ Nickvision.Desktop.WinUI/Converters/IntToBoolConverter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace Nickvision.Desktop.WinUI.Converters;
+
+public class IntToBoolConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, string language)
+    {
+        if (value is int intValue)
+        {
+            return intValue != 0;
+        }
+        return false;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language) => throw new NotImplementedException();
+}
