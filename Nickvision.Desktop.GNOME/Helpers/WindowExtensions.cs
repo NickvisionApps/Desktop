b4868449b8cf457229b654c383d013e83ee40e95
Nick Logozzo <nlogozzo225@gmail.com>
2026-01-16 15:44:14 -0500

    V2026.1.0-gnome

---
+++ Nickvision.Desktop.GNOME/Helpers/WindowExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Nickvision.Desktop.Application;
+
+namespace Nickvision.Desktop.GNOME.Helpers;
+
+public static class WindowExtensions
+{
+    extension(Gtk.Window window)
+    {
+        public WindowGeometry WindowGeometry
+        {
+            get
+            {
+                window.GetDefaultSize(out int width, out int height);
+                return new WindowGeometry(width, height, window.IsMaximized());
+            }
+
+            set
+            {
+                if (value.IsMaximized)
+                {
+                    window.Maximize();
+                }
+                else
+                {
+                    window.SetDefaultSize(value.Width, value.Height);
+                }
+            }
+        }
+    }
+}
