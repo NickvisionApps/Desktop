b39bf39dca2b763647beff812fe756bcc560f457
Nick Logozzo <nlogozzo225@gmail.com>
2026-02-07 20:15:43 -0500

    feat: Add `NullToDefaultValueConverter`

---
+++ Nickvision.Desktop/Converters/NullToDefaultValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Nickvision.Desktop.Converters;
+
+public class NullToDefaultValueConverter<T> : JsonConverter<T> where T : struct, Enum
+{
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return default;
+        }
+        return JsonSerializer.Deserialize<T>(ref reader, options);
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+}
