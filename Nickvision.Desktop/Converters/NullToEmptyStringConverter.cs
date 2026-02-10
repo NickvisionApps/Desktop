97128ed8ec2bb13a1ff608480d8fb0e2d19944da
Nick Logozzo <nlogozzo225@gmail.com>
2026-02-09 21:03:30 -0500

    feat: Add more null json converters

---
+++ Nickvision.Desktop/Converters/NullToEmptyStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Nickvision.Desktop.Converters;
+
+public class NullToEmptyStringConverter : JsonConverter<string>
+{
+    public override string Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return string.Empty;
+        }
+        return JsonSerializer.Deserialize<string>(ref reader, options) ?? string.Empty;
+    }
+
+    public override void Write(Utf8JsonWriter writer, string value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+}
---
+++ Nickvision.Desktop/Converters/NullToFalseBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Nickvision.Desktop.Converters;
+
+public class NullToFalseBoolConverter : JsonConverter<bool>
+{
+    public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return false;
+        }
+        return JsonSerializer.Deserialize<bool>(ref reader, options);
+    }
+
+    public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+}
