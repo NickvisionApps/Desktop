934c8ce01b81b738ce3ab8929c5fc454aecb4914
Nick Logozzo <nlogozzo225@gmail.com>
2026-02-10 20:48:52 -0500

    V2026.2.3

---
+++ Nickvision.Desktop/Converters/NullToDefaultObjectConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Nickvision.Desktop.Converters;
+
+public class NullToDefaultObjectConverter<T> : JsonConverter<T>
+{
+    public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return Activator.CreateInstance<T>();
+        }
+        return JsonSerializer.Deserialize<T>(ref reader, options) ?? Activator.CreateInstance<T>();
+    }
+
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options) => JsonSerializer.Serialize(writer, value, options);
+}
