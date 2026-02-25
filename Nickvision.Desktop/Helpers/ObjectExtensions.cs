40598ccd1b20462a6f387efec826b906b25485c3
Nick Logozzo <nlogozzo225@gmail.com>
2026-02-24 21:12:04 -0500

    V2026.2.12

---
+++ Nickvision.Desktop/Helpers/ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Nickvision.Desktop.Helpers;
+
+public static class ObjectExtensions
+{
+    extension<T>(T obj)
+    {
+        public T DeepCopy() => JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(obj))!;
+    }
+}
+
