50aa72c3a57395b0f966f44bcd29dab152c449de
Nick Logozzo <nlogozzo225@gmail.com>
2026-02-25 22:59:39 -0500

    Revert "V2026.2.13"
    This reverts commit 2480f0bbdc9ad1e635b521f4993f87ad561d1029.

--- Nickvision.Desktop.GNOME/Helpers/MacOSImports.cs
+++ Nickvision.Desktop.GNOME/Helpers/MacOSImports.cs
@@ -4,9 +4,9 @@ namespace Nickvision.Desktop.GNOME.Helpers;
 
 public static partial class MacOSImports
 {
-    [LibraryImport("libgobject-2.0.0.dylib", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("libgobject-2.0.dylib", StringMarshalling = StringMarshalling.Utf8)]
     public static partial ulong g_signal_connect_data(nint instance, string signal, WindowExtensions.OpenCallback callback, nint data, nint destroy_data, int flags);
 
-    [LibraryImport("libgio-2.0.0.dylib", StringMarshalling = StringMarshalling.Utf8)]
+    [LibraryImport("libgio-2.0.dylib", StringMarshalling = StringMarshalling.Utf8)]
     public static partial string g_file_get_uri(nint file);
 }
