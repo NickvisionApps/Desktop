627dc5b4625cc9f300423fdfc6c73069d876917c
Nick Logozzo <nlogozzo225@gmail.com>
2026-03-01 14:06:03 -0500

    V2026.3.0

--- Nickvision.Desktop/Helpers/HostApplicationBuilderExtensions.cs
+++ Nickvision.Desktop/Helpers/HostApplicationBuilderExtensions.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Nickvision.Desktop.Application;
 using Nickvision.Desktop.Filesystem;
 using Nickvision.Desktop.Globalization;
 using Nickvision.Desktop.Keyring;
 using Nickvision.Desktop.Notifications;
 using Nickvision.Desktop.System;
+using NReco.Logging.File;
 
 namespace Nickvision.Desktop.Helpers;
 
@@ -13,7 +15,7 @@ public static class HostApplicationBuilderExtensions
 {
     extension(IHostApplicationBuilder builder)
     {
-        public IHostApplicationBuilder ConfigureNickvision(string[] args)
+        public IHostApplicationBuilder ConfigureNickvision(string[] args, string loggingPath)
         {
             builder.Services.AddSingleton<IArgumentsService>(new ArgumentsService(args));
             builder.Services.AddSingleton<IJsonFileService, JsonFileService>();
@@ -24,6 +26,13 @@ public static class HostApplicationBuilderExtensions
             builder.Services.AddSingleton<ITranslationService, TranslationService>();
             builder.Services.AddSingleton<IUpdaterService, UpdaterService>();
             builder.Services.AddHttpClient<IUpdaterService, UpdaterService>();
+            builder.Logging.ClearProviders();
+            builder.Logging.SetMinimumLevel(LogLevel.Information);
+            builder.Logging.AddConsole();
+            if(!string.IsNullOrEmpty(loggingPath))
+            {
+                builder.Logging.AddFile(loggingPath);
+            }
             return builder;
         }
     }
