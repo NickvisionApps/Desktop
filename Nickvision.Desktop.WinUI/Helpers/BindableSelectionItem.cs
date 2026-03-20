4331c99b6c65b3702c87aac36a3305a9a006aa8a
Nick Logozzo <nlogozzo225@gmail.com>
2026-03-20 14:46:49 -0400

    V2026.3.3-winui

---
+++ Nickvision.Desktop.WinUI/Helpers/BindableSelectionItem.cs
@@ -0,0 +1,26 @@
+﻿using Nickvision.Desktop.Application;
+using WinRT;
+
+namespace Nickvision.Desktop.WinUI.Helpers;
+
+[GeneratedBindableCustomProperty]
+public sealed partial class BindableSelectionItem
+{
+    private readonly ISelectionItem _selectionItem;
+
+    public BindableSelectionItem(ISelectionItem selectionItem)
+    {
+        _selectionItem = selectionItem;
+    }
+
+    public string Label => _selectionItem.Label;
+
+    public bool ShouldSelect
+    {
+        get => _selectionItem.ShouldSelect;
+
+        set => _selectionItem.ShouldSelect = value;
+    }
+
+    public static implicit operator BindableSelectionItem(SelectionItem<object> item) => new BindableSelectionItem(item);
+}
